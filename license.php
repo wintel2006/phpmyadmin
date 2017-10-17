87e88cfd296a188c96182fb7b1127dc523fd6884
test <test@abc.com>
2017-10-16 22:12:22 -0700

    modify license.php

--- license.php
+++ license.php
@@ -2,7 +2,7 @@
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 /**
  * Simple script to set correct charset for the license
- *
+ * asdflasdfjsaldfjasl
  * Note: please do not fold this script into a general script
  * that would read any file using a GET parameter, it would open a hole
  *
